0d0ad448d346d8a3fa294ca212e57ae403dafe99
“gmhislop” <g.hislop@live.nl>
2023-03-06 23:24:16 +0100

    set up pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetUps';
+import NewMeetUpPage from './pages/NewMeetUp';
+import FavoritesPage from './pages/Favorites';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route path="/">
+        <AllMeetupsPage />
+      </Route>
+      <Route path="/new-meetups">
+        <NewMeetUpPage />
+      </Route>
+      <Route path="/favorite-meetups">
+        <FavoritesPage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
