067b95e74be4a8271b9de8a0d4d6fc820ab26a3e
“gmhislop” <g.hislop@live.nl>
2023-03-09 14:39:41 +0100

    fix: format code

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -3,7 +3,6 @@ import { useContext } from 'react';
 import styles from './MainNavigation.module.css';
 import FavoritesContext from '../../store/favorites-context';
 
-
 const MainNavigation = () => {
   const favoritesCtx = useContext(FavoritesContext);
   return (
@@ -12,14 +11,16 @@ const MainNavigation = () => {
       <nav>
         <ul>
           <li>
-            <Link to='/'>All Meetups</Link>
+            <Link to="/">All Meetups</Link>
           </li>
           <li>
-            <Link to='/new'>Add New Meetup</Link>
+            <Link to="/new">Add New Meetup</Link>
           </li>
           <li>
-            <Link to='/favorites'>Favorites
-            <span className={styles.badge}>{favoritesCtx.totalFavorites}</span></Link>
+            <Link to="/favorites">
+              Favorites
+              <span className={styles.badge}>{favoritesCtx.totalFavorites}</span>
+            </Link>
           </li>
         </ul>
       </nav>
